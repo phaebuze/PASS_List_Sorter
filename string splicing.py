829fc5498b8b87367bd68260e21706f13c90328b
Phaedra Buzek <124410661+phaebuze@users.noreply.github.com>
2023-11-13 12:54:55 -0500

    Nov. 13 - String splicing
    Not finished

---
+++ string splicing.py
@@ -0,0 +1,23 @@
+string = "hello, world!"
+stringLength = 12
+
+# print just the words
+print(string[0:5] + " " + string[7:12])
+
+# print string backwords
+for i in range(stringLength, -1, -1):
+  print(string[i])
+
+# convert to uppercase
+print(string.upper())
+
+# capitalize first letter
+# string[0].upper()
+print(string)
+
+# starts with 'hello'
+startsWith = "hello"
+
+for i in range(0, srtingLength + 1):
+  
+
